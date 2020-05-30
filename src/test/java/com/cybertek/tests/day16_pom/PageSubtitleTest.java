418cb23f9b442a288b3e49662f53bd24cd6f31f6
“ahmet1972” <ahmeterdogan94@gmail.com>
2020-05-30 22:53:18 +0100

    new utility class

--- src/test/java/com/cybertek/tests/day16_pom/PageSubtitleTest.java
+++ src/test/java/com/cybertek/tests/day16_pom/PageSubtitleTest.java
@@ -8,38 +8,30 @@ import com.cybertek.utilities.BrowserUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class PageSubtitleTest {
-    /**
-     * Test case
-     * Open browser
-     * Login as a Driver
-     * Verify that page subtitle is Quick Launchpad
-     * Go to Activities -> Calendar Events
-     * verify that page subtitle is Calendar Events
-     */
+public class PageSubtitleTest extends TestBase {
 
     @Test
-    public void test1(){
-        LoginPage loginPage = new LoginPage();
+    public void test1() {
 
-        BrowserUtils.waitFor(30);
+        LoginPage loginPage = new LoginPage();
         loginPage.loginAsDriver();
-        //Same thing with thread.sleep(3000)
-        //BrowserUtils.waitFor(10);
+
+        BrowserUtils.waitFor(3);
+
         DashboardPage dashboardPage = new DashboardPage();
 
-        String expectedSubtitle = "Quick Launchpad";
+        String expectedSubtitle= "Quick Launchpad";
+        String actualSubtitle= dashboardPage.getPageSubTitle();
 
-        String actualSubtitle = dashboardPage.getPageSubTitle();
-
-        Assert.assertEquals(actualSubtitle,expectedSubtitle,"verify subtitles");
+        Assert.assertEquals(actualSubtitle,expectedSubtitle,"verify subtitle");
 
         dashboardPage.navigateToModule("Activities","Calendar Events");
 
         CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
-        BrowserUtils.waitFor(5);
 
-        Assert.assertEquals(calendarEventsPage.getPageSubTitle(),"Calendar Events","verify subtitles");
+        BrowserUtils.waitFor(2);
+
+        Assert.assertEquals(calendarEventsPage.getPageSubTitle(),"Calendar Events","verify events");
 
     }
 }
